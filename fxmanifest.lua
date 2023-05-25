a8e6aba2918c97eb53bc3aeee5ead970f805f2cb
Djorr <djorraw@gmail.com>
2023-05-25 09:19:27 +0200

    RDM-Checker script

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game { 'rdr3', 'gta5' }
+
+author 'Djorr <contact@rubixstudios.nl>'
+description 'Just a simple RDM checker'
+version '1.0.0'
+
+server_script 'server.lua'
---
+++ server.lua
@@ -0,0 +1,18 @@
+﻿RegisterNetEvent('onPlayerDeath')
+AddEventHandler('onPlayerDeath', function(data)
+    local victimName = GetPlayerName(PlayerId()) -- Assuming you want to get the victim's name
+    local victimPed = PlayerPedId()
+    local killerPed = GetPedSourceOfDeath(victimPed)
+
+    if IsEntityAVehicle(killerPed) then
+        local killerVehicle = GetVehiclePedIsIn(killerPed, false)
+        local driverName = GetPlayerName(NetworkGetPlayerIndexFromPed(killerPed)) -- Assuming you want to get the driver's name
+
+        -- Perform additional checks if necessary
+
+        -- Print the message with the names
+        print("Player "..victimName.." has been killed by "..driverName..".")
+
+        -- Could give them a /reviveme command
+    end
+end)

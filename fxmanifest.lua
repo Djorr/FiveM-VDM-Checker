4bdc512d1dc4a2289a4795a2e723ddc69a454726
Djorr <djorraw@gmail.com>
2023-05-25 09:20:43 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game { 'rdr3', 'gta5' }
 
 author 'Djorr <contact@rubixstudios.nl>'
-description 'Just a simple RDM checker'
+description 'Just a simple VDM checker'
 version '1.0.0'
 
 server_script 'server.lua'
